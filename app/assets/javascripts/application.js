4f1ca49189abd305030a47691b95af6a0ed35275
Ollie Pegram <olliepegram@gmail.com>
2016-09-09 18:02:06 +1000

    final site

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+function scrollTo() {
+       $('html, body').animate({ scrollTop: $('#about').offset().top }, 'slow');
+       return false;
+   }
