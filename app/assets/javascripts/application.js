852dcdb2ad740f9a9e0e675e4a9fab38ac3e085f
Ollie Pegram <olliepegram@gmail.com>
2016-09-12 14:55:18 +1000

    another one

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
 
 function scrollTo() {
        $('html, body').animate({ scrollTop: $('#about').offset().top }, 'slow');
